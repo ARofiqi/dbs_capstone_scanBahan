410ca93b88094591931b4f09b981bf1ae1a8d364
Rofiqi2345 <ahmadrofiqi333@gmail.com>
2025-06-07 23:51:56 +0700

    update

--- server/index.js
+++ server/index.js
@@ -4,17 +4,17 @@ const Hapi = require("@hapi/hapi");
 const routes = require("./routes");
 const config = require("./config/default");
 const path = require("path");
-const fs = require("fs");
+// const fs = require("fs");
 
 const init = async () => {
   const server = Hapi.server({
     port: config.port,
     host: "localhost",
-    tls: process.env.NODE_ENV === 'production' ? {
-      key: fs.readFileSync("/home/ec2-user/ssl/key.pem"),
-      cert: fs.readFileSync("/home/ec2-user/ssl/cert.pem"),
-    } : null,
   });
+  // tls: process.env.NODE_ENV === 'production' ? {
+  //   key: fs.readFileSync("/home/ec2-user/ssl/key.pem"),
+  //   cert: fs.readFileSync("/home/ec2-user/ssl/cert.pem"),
+  // } : null,
 
   await server.register(require("@hapi/inert"));
 
